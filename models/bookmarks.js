89cb9c5845204f0d8662c7730114a5813d587c58
Satan <coutamit.s19@gmail.com>
2020-10-24 00:16:34 +0530

    update

--- models/bookmarks.js
+++ models/bookmarks.js
@@ -1,5 +1,12 @@
-const { Timestamp } = require("bson");
 var mongoose = require("mongoose");
+require("dotenv").config();
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((err) => console.log(err));
 
 
 var Schema = new mongoose.Schema(
--- models/tags.js
+++ models/tags.js
@@ -1,4 +1,13 @@
 var mongoose = require("mongoose");
+require("dotenv").config();
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((err) => console.log(err));
+
 
 var Schema = new mongoose.Schema(
     {
